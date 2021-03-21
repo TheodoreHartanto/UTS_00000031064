1615a94841f12d11d0d696c9952fa255cf777bff
TheodoreHartanto <theodore.alvin@student.umn.ac.id>
2021-03-21 20:13:26 +0700

    Fourth Commit

---
+++ gradle/app/src/main/java/umn/ac/id/uts_00000031064_theodorealvinhartanto/ProfileLoggedActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.uts_00000031064_theodorealvinhartanto;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileLoggedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
